df54500b48ea4d495f11c29ead47b1ea6fc16023
Neetu Mittal <shabnikkigoyal@gmail.com>
2025-08-20 13:08:12 +0530

    fix: update backend and login links for ner stage in deployment workflow

--- web/src/config.ts
+++ web/src/config.ts
@@ -10,8 +10,8 @@ const config = {
   mapBoxToken: process.env.REACT_APP_MAPBOXGL_ACCESS_TOKEN,
   mapType: process.env.REACT_APP_MAP_TYPE || 'None',
   iframurl: process.env.REACT_APP_IFRAME_BASE_URL || 'http://localhost:3000/carbon/app',
-  carbonLogin: process.env.REACT_APP_CARBON_LOGIN_LINK || 'https://carbon-admin.mtckarn7q2.com',
-  bioLogin: process.env.REACT_APP_BIO_LOGIN_LINK || 'https://bio-admin.mtckarn7q2.com',
+  carbonLogin: process.env.REACT_APP_CARBON_LOGIN_LINK || 'https://admin.carbon.bahamasemissionregistry.org',
+  bioLogin: process.env.REACT_APP_BIO_LOGIN_LINK || 'https://admin.bio.bahamasemissionregistry.org',
 };
 
 export default config;
